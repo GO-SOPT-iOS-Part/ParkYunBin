b6c4f19cba73cb1f0a257e3e1327231a0c5ee29d
binisnull <dbsqlssss0704@gmail.com>
2023-04-28 19:16:35 +0900

    [FIX] #2 UIStackView+ 미사용으로 파일 삭제

--- SOPT_iOS_Week2/SOPT_iOS_Week2/Global/Extension/UIFont+.swift
+++ SOPT_iOS_Week2/SOPT_iOS_Week2/Global/Extension/UIFont+.swift
@@ -7,28 +7,6 @@
 
 import UIKit
 
-//extension UIFont {
-//
-//    class func regular(size: CGFloat) -> UIFont {
-//        return UIFont(name: AppFontName.regularFont.rawValue, size: size)!
-//    }
-//
-//    class func medium(size: CGFloat) -> UIFont {
-//        return UIFont(name: AppFontName.mediumFont.rawValue, size: size)!
-//    }
-//
-//    class func semiBold(size: CGFloat) -> UIFont {
-//        return UIFont(name: AppFontName.semiBoldFont.rawValue, size: size)!
-//    }
-//
-//    class func bold(size: CGFloat) -> UIFont {
-//        return UIFont(name: AppFontName.boldFont.rawValue, size: size)!
-//    }
-//
-//}
-
-import UIKit
-
 extension UIFont {
     
         class func regular(size: CGFloat) -> UIFont {
